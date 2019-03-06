8dbfdb8edefffd324b877edf5feb6f8d6244a962
Evgenii Nikitin <en919@nyu.edu>
2019-03-06 15:02:30 -0500

    change flask redirect

--- elasticsmapp/app/views.py
+++ elasticsmapp/app/views.py
@@ -9,7 +9,6 @@ elastic_server_baseurl = 'http://localhost:9200/'
 
 
 @app.route('/kibana', methods=['POST', 'GET'])
-@requires_auth
 def kibana():
     req = requests.get(kibana_server_baseurl, stream=True)
     return Response(stream_with_context(req.iter_content()),
