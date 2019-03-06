dc11400e981e0f63e97f91612a3f20ad20a1afd7
Evgenii Nikitin <en919@nyu.edu>
2019-03-06 14:59:32 -0500

    change flask redirect

--- elasticsmapp/app/views.py
+++ elasticsmapp/app/views.py
@@ -1,16 +1,24 @@
-from flask import redirect, url_for
+import requests
 
 from elasticsmapp.app.app import app
 from elasticsmapp.app.auth import requires_auth
+from flask import Response, stream_with_context
+
+kibana_server_baseurl = 'http://localhost:5601/'
+elastic_server_baseurl = 'http://localhost:9200/'
 
 
 @app.route('/kibana', methods=['POST', 'GET'])
 @requires_auth
 def kibana():
-    return redirect(url_for("http://localhost:5601"))
+    req = requests.get(kibana_server_baseurl, stream=True)
+    return Response(stream_with_context(req.iter_content()),
+                    content_type=req.headers['content-type'])
 
 
 @app.route('/elastic', methods=['POST', 'GET'])
 @requires_auth
 def elastic():
-    return redirect(url_for("http://localhost:9200"))
+    req = requests.get(elastic_server_baseurl, stream=True)
+    return Response(stream_with_context(req.iter_content()),
+                    content_type=req.headers['content-type'])
