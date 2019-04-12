31109a8362208863298856c6ef972eb72e23c171
Evgenii Nikitin <en919@nyu.edu>
2019-04-12 12:07:37 -0400

    change op_type to update

--- elasticsmapp/utils/put_data.py
+++ elasticsmapp/utils/put_data.py
@@ -58,7 +58,8 @@ def put_data_from_json(index_name, filename, platform='reddit',
             actions = [
                 {
                     "_index": index_name,
-                    '_op_type': 'create',
+                    '_op_type': 'update',
+                    "doc_as_upsert": True,
                     "_type": '_doc',
                     "_id": str(post[id_field]),
                     "_source": post
