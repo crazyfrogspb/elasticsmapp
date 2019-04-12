732c6a24a394406a6677c57278f8701fb8eb29d8
Evgenii Nikitin <en919@nyu.edu>
2019-04-12 17:37:36 -0400

    remove stopwords when calculating embeddings

--- elasticsmapp/utils/embeddings.py
+++ elasticsmapp/utils/embeddings.py
@@ -1,12 +1,14 @@
 import base64
 
-import numpy as np
-
 import gensim.downloader as api
+import numpy as np
+from nltk.corpus import stopwords
+
 
 dbig = np.dtype('>f8')
 model = api.load("glove-twitter-25")
 num_features = model.wv.vector_size
+stopwords = set(stopwords.words('english'))
 
 
 def decode_float_list(base64_string):
@@ -19,12 +21,12 @@ def encode_array(arr):
     return base64_str
 
 
-def get_embedding(sentence):
+def get_embedding(sentence, ignore_stopwords=True):
     words = sentence.lower().split()
     feature_vec = np.zeros((num_features, ), dtype='float32')
     n_words = 0
     for word in words:
-        if word in model.vocab:
+        if word in model.vocab and word not in stopwords:
             n_words += 1
             feature_vec = np.add(feature_vec, model[word])
     if n_words > 0:
