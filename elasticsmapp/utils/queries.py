c7208905608421255bfe7c4cd2feb83d9653341f
Evgenii Nikitin <en919@nyu.edu>
2019-05-01 16:56:29 -0400

    new app

--- elasticsmapp/utils/queries.py
+++ elasticsmapp/utils/queries.py
@@ -3,7 +3,7 @@ import http.client
 from elasticsearch import Elasticsearch
 from elasticsmapp.utils.embeddings import get_embedding
 
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+es = Elasticsearch([{'host': '192.168.0.71', 'port': None}])
 http.client._MAXHEADERS = 10000
 
 
@@ -11,7 +11,7 @@ def find_similar_documents(sentence, subreddit, index_name='reddit', size=100):
     embedding_vector = get_embedding(sentence)
     query = {'query': {
         "function_score": {
-            "query": {"term": {"subreddit": subreddit}},
+            "query": {"range": {"created_utc": {"gte": "01/10/2018", "lte": "01/10/2018", "format": "dd/MM/yyyy"}}},
             "boost_mode": "replace",
             "functions": [
                 {
@@ -32,4 +32,5 @@ def find_similar_documents(sentence, subreddit, index_name='reddit', size=100):
     },
         "size": size
     }
+
     return es.search(index=index_name, doc_type='_doc', body=query)
